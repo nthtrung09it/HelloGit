abf48dbf9b7241b70db27cd00956c6c1822aa7ed
Trung Nguyen <trung.nguyen@dounets.com>
2016-06-02 17:08:39 +0700

    ABC

--- app/src/main/java/nthtrung09it/hellogit/MainActivity.java
+++ app/src/main/java/nthtrung09it/hellogit/MainActivity.java
@@ -1,7 +1,9 @@
 package nthtrung09it.hellogit;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText("This is my textg");
 
     }
 }
